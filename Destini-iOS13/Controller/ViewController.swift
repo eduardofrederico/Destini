2661cc9faffaaddf5af49e7597fb8e1383af695b
eduardofrederico <eduardo.frederico@icloud.com>
2022-08-13 09:51:36 -0300

    First Comiit

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,9 +14,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let stories = [
+        
+        Story(title: "You see a fork in the road", choice1: "Take a left.", choice2: "Take a right."),
+        Story(title: "You see a tiger", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest", choice1: "Open it.", choice2: "Check for traps.")
+    
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateUI()
 
+    }
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let actualStory = stories[0].title
+        
+    }
+    
+    func updateUI() {
+        storyLabel.text = stories[0].title
     }
 
 
